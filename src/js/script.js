3cde14e02d69a0fb07e4ab0685a6019240d9e1b7
Denis <denis@gmail.com>
2024-11-27 17:40:34 +0200

    portfolio, price and contacts sections

--- src/js/script.js
+++ src/js/script.js
@@ -14,17 +14,8 @@ closeElem.addEventListener('click', function () {
     menu.classList.remove('menu_active');
 })
 
-const scale = document.querySelector('.grade__item-sclae');
-const innerSclae = document.querySelector('.grade__item-sclae-inner');
-
-// console.log(scale);
-
 const counter = document.querySelectorAll('.grade__item-percent');
 const lines = document.querySelectorAll('.grade__item-scale span');
-
-// counter.forEach((item, i) => {
-//     lines[i].style.width = item.innerHTML;
-// });
 
 counter.forEach(function (item, i) {
     lines[i].style.width = item.innerHTML;
